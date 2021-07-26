0345f435e0090287af83dee73f10db1cd72af381
Nidhi Verma <nidhiverma.mca19.du@gmail.com>
2021-07-26 19:04:16 +0530

    Time: 2 ms (76.31%), Space: 37 MB (89.66%) - LeetHub

--- reorganize-string/reorganize-string.java
+++ reorganize-string/reorganize-string.java
@@ -1,39 +1,59 @@
 class Solution {
-    public String reorganizeString(String s) {
-        int[] hash = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            hash[s.charAt(i) - 'a']++;
-        } 
-        int max = 0, letter = 0;
-        for (int i = 0; i < hash.length; i++) {
-            if (hash[i] > max) {
-                max = hash[i];
-                letter = i;
-            }
+    class Pair {
+        char c;
+        int f;
+        public Pair(char c, int f) {
+            this.c = c;
+            this.f = f;
         }
+    }
+    public String reorganizeString(String s) {
+        int[] freq = new int[26];
+        char[] chars = s.toCharArray();
+        
+        int max = 0, letter = 0;
+        
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            freq[c - 'a']++;
+            max = Math.max(max, freq[c-'a']);
+            if(max == freq[c-'a']) letter = i;
+        } 
+        
+        // can't be reorganized
         if (max > (s.length() + 1) / 2) {
             return ""; 
         }
-        char[] res = new char[s.length()];
-        int idx = 0;
         
-        while (hash[letter] > 0) {
-            res[idx] = (char) (letter + 'a');
-            idx += 2;
-            hash[letter]--;
-        }
-        
-        for (int i = 0; i < hash.length; i++) {
-            while (hash[i] > 0) {
-                if (idx >= res.length) {
-                    idx = 1;
-                }
-                res[idx] = (char) (i + 'a');
-                idx += 2;
-                hash[i]--;
+        PriorityQueue<Pair> pq = new PriorityQueue(new Comparator<Pair>() {
+            public int compare(Pair a, Pair b) {
+                return b.f - a.f;
+            }
+        });
+                                                   
+        for(int i = 0; i < 26; i++) {
+            if(freq[i] > 0) {
+                pq.offer(new Pair((char)(i+'a'), freq[i]));
             }
         }
-        return String.valueOf(res);
 
+        StringBuilder sb = new StringBuilder();
+        while(pq.size() > 1) {
+            Pair curr = pq.poll();
+            Pair next = pq.poll();
+            sb.append(curr.c);
+            sb.append(next.c);
+            freq[curr.c-'a']--;
+            freq[next.c-'a']--;
+            
+            if(freq[curr.c-'a'] > 0) {
+                pq.offer(new Pair(curr.c, freq[curr.c-'a']));
+            }
+            if(freq[next.c-'a'] > 0) {
+                pq.offer(new Pair(next.c, freq[next.c-'a']));
+            }
+        }
+        if(pq.size() > 0) sb.append(pq.poll().c);
+        return sb.toString();
     }
 }
