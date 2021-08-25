b0ee84d07c8e88e8292deb1893b2ecfed3607b45
Nidhi Verma <nidhiverma.mca19.du@gmail.com>
2021-08-26 00:37:40 +0530

    Time: 37 ms (21.54%), Space: 41.5 MB (7.68%) - LeetHub

--- 4sum/4sum.java
+++ 4sum/4sum.java
@@ -1,35 +1,40 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList();
+        
+        if(nums.length < 4) return result;
+        
         Arrays.sort(nums);
         
-        List<List<Integer>> res = new ArrayList();
         for(int i = 0; i < nums.length-3; i++) {
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            
             for(int j = i+1; j < nums.length-2; j++) {
                 if(j > i+1 && nums[j] == nums[j-1]) continue;
                 
-                int lo = j+1, hi = nums.length-1;
+                int left = j+1, right = nums.length-1;
+                int partialSum = nums[i] + nums[j];
                 
-                while(lo < hi) {
-                    int sum = nums[i] + nums[j] + nums[lo] + nums[hi];
+                while(left < right) {
+                    int sum = nums[left] + nums[right] + partialSum;
+                    
                     if(sum == target) {
-                        res.add(new ArrayList(Arrays.asList(nums[i], nums[j], nums[lo], nums[hi])));
-                        lo++;
-                        hi--;
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
                         
-                        while(lo < hi && nums[lo] == nums[lo-1]) lo++;
-                        while(lo < hi && nums[hi] == nums[hi+1]) hi--;
+                        while(left < right && nums[left] == nums[left+1]) left++;
+                        while(left < right && nums[right] == nums[right-1]) right--;
+                       
+                        left++;
+                        right--;
                     }
                     else if(sum < target) {
-                        lo++;
+                        left++;
                     }
                     else {
-                        hi--;
+                        right--;
                     }
                 }
             }
         }
-        return res;
+        return result;
     }
 }
